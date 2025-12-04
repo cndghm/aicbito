1f09bdd918d4089766448b942cf0b2bb908e2c08
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2025-12-04 14:30:59 -0300

    TcpStream connects

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let stream = TcpStream::connect("irc.libera.chat:6667").await?;
+    println!("Connected.");
+    Ok(())
 }
